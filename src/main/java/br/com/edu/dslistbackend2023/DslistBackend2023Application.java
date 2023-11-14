065156ae0fcb6a9f9e267ff058040d18b4ed7a89
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2023-11-14 16:39:37 -0300

    Projeto criado

---
+++ src/main/java/br/com/edu/dslistbackend2023/DslistBackend2023Application.java
@@ -0,0 +1,13 @@
+package br.com.edu.dslistbackend2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistBackend2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DslistBackend2023Application.class, args);
+    }
+
+}
